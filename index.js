507226ffff0bd541b7a2f0c238056e0ad845bff7
cludley <cludley@anamosa.k12.ia.us>
2025-12-09 19:04:02 +0000

    n

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function calculateSquare(){
     let value = document.getElementById('number').value;
     let result = document.getElementById('result');
+    const num = parseFloat(value);
 
-    //add code here
-    
+    if (!isFinite(num)) {
+        result.textContent = "Invalid, please enter a number";
+        return;
+    }
+
+    const squared = num * num;
+    result.textContent = squared;
 }
